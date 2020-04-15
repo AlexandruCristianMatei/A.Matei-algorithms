f42e702b968ceab71efd15b234f09a3bd64cde5f
Vlad Vedinas <vlad. vedinas@scoalainformala.ro>
2020-04-15 23:09:54 +0300

    A.M.algo/rithm

---
+++ main.js
@@ -0,0 +1,77 @@
+// 1. display in the console the numbers from 1 to 20
+
+// var numbers;
+// var string = function string(numbers) {
+//     for (i = 1; i <= 20; i++) {
+//         console.log(i);
+//     }
+// }
+
+// string(numbers);
+
+// 2. display in the console the odd numbers from 1 to 20
+
+// var oddNum;
+
+// function odd(oddNum) {
+//     for (i = 1; i <= 20; i++) {
+//         if (i % 2 != 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// odd(oddNum);
+
+//3. compute the sum of the elements of an array and display it in the console
+
+// let x = [2, 6, 34, 50, -23, 0, 80, -66, -80];
+// let sum = 0;
+// let add;
+
+// function total(add) {
+//     for (i = 0; i < x.length; i++) {
+//         sum = sum + x[i];
+//         console.log(sum);
+//     }
+// }
+
+// total(add);
+
+//4. compute the maximum of the elements of an array and display it in the console
+
+// let x = [37, 55, 99, 123, 200, 28, 11, 433, 15];
+// let max = x[0];
+// let maximum;
+
+// function rampup(maximum) {
+//     for (i = 0; i < x.length; i++) {
+//         if (max < x[i + 1]) {
+//             max = x[i + 1];
+//         } else if (max < x[i + 1]) {
+//             max = x[i];
+//         }
+//     }
+//     console.log(max);
+// }
+
+// rampup(maximum);
+
+//5. compute how many times a certain element appears in an array
+
+// var x = [2, 4, 6, 12, 33, 9, 12, 10, 20, 2, 2, 12, 23, 2, 15, 2, 2, 2, 2];
+// var i = 0;
+// var counter = 0;
+// var element = 12;
+// var repeat;
+
+// function learn(repeat) {
+//     for (i; i < x.length; i++) {
+//         if (x[i] === element) {
+//             counter = counter + 1;
+//         }
+//     }
+//     console.log(counter);
+// }
+
+// learn(repeat);
